70d9477d7f38766671523c9137ba8aff8f100934
Methychella Panga <149224940+Methychella@users.noreply.github.com>
2025-03-16 05:28:58 +0000

    troisieme  modifications

--- Contact/server.js
+++ Contact/server.js
@@ -1,36 +1,36 @@
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+const path = require('path');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-//Middleware
+// Middleware pour servir des fichiers statiques
+app.use(express.static(path.join(__dirname)));
+
+// Middleware pour les requetes JSON et URL- encoded
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
-//Route pour envoyer le formulaire à Formspree
-app.post('/send-email', async(req, res) =>{
+app.post('/contact', async (req, res) =>{
     const {name, email, comment} = req.body;
     
-    if (!name || !email || !comment) {
-        return res.status(400).json({error: "Tous le champs sont obligatoires"});
-    }
     try{
-        const response = await axios.post('https://formspree.io/f/mblgllyn', {name, email, comment}, {headers: {'Accept': 'application/json'}});
-        if (response.status === 200) {
-            res.status(200).json({comment: "Messange envoyé avec succés"});
+        const response = await axios.post('https://formspree.io/f/mblgllyn', {name, email, comment}, {headers: {'Accept': 'application/json',},});
+        // Verifie si la requete a réussi
+        if (response === 200) {
+            return res.json({message: 'Messange envoyé avec success !'});
         }else{
-            res.status(500).json({error: "Erreur lors de l'envoi du messange"});
+            return res.status(response.status).json({message: 'Erreur los de l\'envoi.'});
         }
     }catch(error){
-        console.error("Erreur Formspree:", error);
-        res.status(500).json({error: "Impossible d'envoyer le messange"});
+        console.error(error);
+        return res.status(500).json({message: 'Erreur serveur'});
     }
+    });
+app.listen(PORT, () =>{
+    console.log(`Serveur à l\'écoute su le port http://localhost:${PORT}`);
 });
-
-//Lance le serveur
-app.listen(port, () =>{
-    console.log(`Serveur en écoute sur https://localhost:${port}`);
-})
